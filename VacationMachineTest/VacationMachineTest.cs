c0ef01259fb2e10dc256a3425b2479b44d77e1f1
Calfur <robin.nater@hotmail.com>
2022-10-29 00:02:37 +0200

    create test project

---
+++ VacationMachineTest/VacationMachineTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VacationMachineTest
+{
+    [TestClass]
+    public class VacationMachineTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void RequestPaidDaysOff()
+        {
+        }
+    }
+}
