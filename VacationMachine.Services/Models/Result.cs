a945a059b0329823c026a72333e84b9ea50eb3e0
LukaszGajda <lgajda1@gmail.com>
2022-11-03 10:34:47 +0100

    save

--- VacationMachine.Services/Models/Result.cs
+++ VacationMachine.Services/Models/Result.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VacationMachine.Services.Models
+﻿namespace VacationMachine.Services.Models
 {
     public enum Result
     {
